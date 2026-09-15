735322a957d79e0e5fdb6a00be25fad1a21c1a1b
Atharv Tandale <atharvstandale@gmail.com>
2026-09-15 13:58:47 +0530

    Update LeetCode #9 - Palindrome Number

--- 0009-palindrome-number/solution.py
+++ 0009-palindrome-number/solution.py
@@ -1,1 +1,11 @@
-c
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp=x
+        rev=0
+
+        while temp>0:
+            r=temp%10
+            temp=temp//10
+            rev=rev*10+r
+        
+        return rev==x
