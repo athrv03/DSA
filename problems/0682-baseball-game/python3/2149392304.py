7a1803bef134b7f7d0b6e22e82cba409ff337d4a
Atharv Tandale <atharvstandale@gmail.com>
2026-09-22 11:42:13 +0530

    Archive LeetCode #682 - Baseball Game (2149392304)

---
+++ problems/0682-baseball-game/python3/2149392304.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        st=[]
+
+        for op in operations:
+            if op=="+":
+                st.append(st[-1]+st[-2])
+            elif op=="D":
+                st.append(2*int(st[-1]))
+            elif op=="C":
+                st.pop()
+            else:
+                st.append(int(op))
+        
+        return sum(st)
